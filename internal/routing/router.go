2457d7a1b159c9dd38e4cbcd84b484f091a81b1d
Shreya Birmecha <shreyabirmecha8@gmail.com>
2025-12-14 20:20:57 +0530

    updated server and router

--- internal/routing/router.go
+++ internal/routing/router.go
@@ -2,9 +2,6 @@ package routing
 
 import "github.com/sbirmecha99/smart-adaptive-load-balancer/internal/core"
 
-// Router defines how a backend server is selected
 type Router interface {
-	GetNextAvailableServer([]*core.Backend) *core.Backend
+	GetNextAvailableServer(backends []*core.Backend) *core.Backend
 }
-
-
