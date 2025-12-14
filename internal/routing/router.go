a270da99c72633b1345cfe9212be90f781a13e2c
Shreya Birmecha <shreyabirmecha8@gmail.com>
2025-12-14 20:09:07 +0530

    make roundrobin thread-safe

--- internal/routing/router.go
+++ internal/routing/router.go
@@ -1,2 +1,10 @@
 package routing
 
+import "github.com/sbirmecha99/smart-adaptive-load-balancer/internal/core"
+
+// Router defines how a backend server is selected
+type Router interface {
+	GetNextAvailableServer([]*core.Backend) *core.Backend
+}
+
+
